7b0040c76c9d63ed6c67ec7c5678daa6f0d0afa1
alberto sanchez <alberto.sanchez@reinamadre.mx>
2024-09-02 20:34:46 -0500

    chore: cleanup project

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,4 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
@@ -8,12 +6,10 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="SI730: You did it!" />
     </div>
   </header>
 
   <main>
-    <TheWelcome />
   </main>
 </template>
 
